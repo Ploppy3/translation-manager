511246d2210ed964f86c5d6bf5b87f5406143f86
Robin <support@robe-medical.com>
2020-02-07 09:49:41 +0100

    update @angular/core

--- src/app/dialog-confirm/dialog-confirm.component.spec.ts
+++ src/app/dialog-confirm/dialog-confirm.component.spec.ts
@@ -1,12 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-import { MaterialModule } from "../material/material.module";
+import { MaterialModule } from '../material/material.module';
 import { DialogConfirmComponent } from './dialog-confirm.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { firebaseConfig } from "../firebaseConfig";
 import { FormsModule } from '@angular/forms';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { AngularFireAuth } from 'angularfire2/auth';
 import { NgModule } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { OverlayContainer } from '@angular/cdk/overlay';
@@ -20,7 +16,6 @@ const TEST_DIRECTIVES = [
     MaterialModule,
     NoopAnimationsModule,
     FormsModule,
-    AngularFireModule.initializeApp(firebaseConfig),
   ],
   exports: TEST_DIRECTIVES,
   declarations: TEST_DIRECTIVES,
@@ -28,8 +23,6 @@ const TEST_DIRECTIVES = [
     DialogConfirmComponent,
   ],
   providers: [
-    AngularFireAuth,
-    AngularFireDatabase,
   ],
 })
 class DialogTestModule { }
@@ -51,13 +44,11 @@ describe('DialogConfirmComponent', () => {
         }
       ]
     });
-    dialog = TestBed.get(MatDialog);
+    dialog = TestBed.inject(MatDialog);
   });
 
   it('should create', () => {
-    let dialogRef = dialog.open(DialogConfirmComponent);
+    const dialogRef = dialog.open(DialogConfirmComponent);
     expect(dialogRef).toBeTruthy();
-    //const button = overlayContainerElement.querySelector('button');
-    //expect(button.textContent).toBe('Annuler');
   });
 });
